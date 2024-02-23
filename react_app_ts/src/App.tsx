05bb514a23bab4867778ecdc562f24663ac295ea
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-02-23 18:38:22 +0700

    update

--- react_app_ts/src/App.tsx
+++ react_app_ts/src/App.tsx
@@ -1,13 +1,26 @@
-import Greet from "./components/Greet"
+import Greet from "./components/Greet";
+import Person from "./components/Person";
 
 function App() {
+  const personName = {
+    first: "Oeurn",
+    last: "Seyha",
+  };
 
+  const personList = [
+    {
+      first: "Oeurn",
+      last: "Seyha",
+    },
+    { first: "Nona", last: "Cola" },
+  ];
   return (
     <>
-    {/* <Greet number={10}/> */}
-    <Greet propName={{"name": "seyha", "number": 10}}/>
+      {/* <Greet great={10}/> */}
+      <Greet propName={{ name: "seyha", number: 10 }} great={10} />
+      <Person name={personName} nameList={personList} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
