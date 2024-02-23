d1006c924acd073cc95de74a6947f7e9857583c0
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-02-23 11:05:27 +0700

    update

--- react_app_ts/src/App.tsx
+++ react_app_ts/src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Greet from "./components/Greet"
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [ counts, setCounts] = useState(2)
+
   return (
     <>
-      
+     <Greet name="seyha"/>
     </>
   )
 }
