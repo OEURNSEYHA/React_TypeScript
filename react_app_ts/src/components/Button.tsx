d67305198d2236bbee5ffcffedcaed9595d56edc
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-02-24 22:04:52 +0700

    update event props

--- react_app_ts/src/components/Button.tsx
+++ react_app_ts/src/components/Button.tsx
@@ -1,18 +1,16 @@
-import Input from "./Input"
+import Input from "./Input";
 
 type ButtonProps = {
-    handleClick : (e: React.MouseEvent<HTMLButtonElement>, id:number) => void
-}
+  handleClick: (e: React.MouseEvent<HTMLButtonElement>, id: number) => void;
+};
 
-
-function Button({handleClick} : ButtonProps) {
+function Button({ handleClick }: ButtonProps) {
   return (
-  <>
-  
-  <button onClick={(e)=> handleClick(e, 20)}> Click </button>
-  <Input  handleChange={(value)=> console.log( value)}/>
-  </> 
-  )
+    <>
+      <button onClick={(e) => handleClick(e, 20)}> Click </button>
+      <Input handleChange={(value) => console.log(value)} />
+    </>
+  );
 }
 
-export default Button
+export default Button;
