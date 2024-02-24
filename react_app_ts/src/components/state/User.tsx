f3681709bd260a881f89aed7ae16db1142a6ac97
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-02-24 23:35:51 +0700

    useState Future value

---
+++ react_app_ts/src/components/state/User.tsx
@@ -0,0 +1,29 @@
+// import React from 'react'
+
+import { useState } from "react";
+type AuthUser = {
+    name: string,
+    email: string
+}
+function User() {
+  const [user, setUser] = useState<AuthUser|null>(null);
+  const handleLogin = () => {
+    setUser({
+        name: "seyha",
+        email: "seyha@gmail.com"
+    })
+  };
+  const handleLogout = () => {
+    setUser(null)
+  };
+
+  return (
+    <div>
+      <h1> user </h1> <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>  User name is {user?.name} <br/> Email is {user?.email}</div>
+    </div>
+  );
+}
+
+export default User;
