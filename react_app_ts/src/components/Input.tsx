56e73fe0f31e404c7f721d98323fcb75b10397d5
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-02-24 22:05:11 +0700

    update event props

--- react_app_ts/src/components/Input.tsx
+++ react_app_ts/src/components/Input.tsx
@@ -1,21 +1,24 @@
 // import React from 'react'
-
 // e: React.ChangeEvent<HTMLInputElement>,
 type InputProps = {
-    // value: string,
-    handleChange: ( value: string) => void
+  // value: string,
+  handleChange: (value: string) => void;
+};
+
+function Input({ handleChange }: InputProps) {
+  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     console.log(e)
+  // }
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(value) => {
+          handleChange(value.target.value);
+        }}
+      />
+    </>
+  );
 }
 
-
-function Input({ handleChange} : InputProps ) {
-    // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     console.log(e)
-    // }
-    return (
-   <>
-    <input type="text" onChange={(value)=>{handleChange(value.target.value)}} />
-   </>
-  )
-}
-
-export default Input
+export default Input;
