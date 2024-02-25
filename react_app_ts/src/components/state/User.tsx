bd106d5f7339e7d08d432cb0a262ec8a14352cbd
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-02-25 10:01:45 +0700

    useState type assertion

--- react_app_ts/src/components/state/User.tsx
+++ react_app_ts/src/components/state/User.tsx
@@ -6,22 +6,23 @@ type AuthUser = {
     email: string
 }
 function User() {
-  const [user, setUser] = useState<AuthUser|null>(null);
+  // const [user, setUser] = useState<AuthUser|null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
   const handleLogin = () => {
     setUser({
         name: "seyha",
         email: "seyha@gmail.com"
     })
   };
-  const handleLogout = () => {
-    setUser(null)
-  };
+  // const handleLogout = () => {
+  //   setUser(null)
+  // };
 
   return (
     <div>
       <h1> user </h1> <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>  User name is {user?.name} <br/> Email is {user?.email}</div>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+      <div>  User name is {user.name} <br/> Email is {user.email}</div>
     </div>
   );
 }
